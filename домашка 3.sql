e58329c575bfdd2041becac19a8bcab143840d2d
FOlkhoffski <148325615+FOlkhoffski@users.noreply.github.com>
2024-05-21 19:04:17 +0300

    Update домашка 3.sql
    first 5 tasks

--- домашка 3.sql
+++ домашка 3.sql
@@ -1,1 +1,7 @@
+1) SELECT COUNT(*) AS Число_учеников FROM pupils
+2) SELECT MIN(score), MAX(score) FROM pupils
+3) SELECT AVG(score) FROM pupils WHERE snum = 123
+4) SELECT AVG(score) FROM pupils GROUP BY (snum)
+5) SELECT schoolno, COUNT(*) FROM pupils GROUP BY snum
+
 
